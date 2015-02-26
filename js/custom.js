cc4398b9275a5903ea74287fb15ab3c732a83733
legiesh <legiesh@gmail.com>
2015-02-26 17:25:32 +0530

    testing

--- js/custom.js
+++ js/custom.js
@@ -11,32 +11,6 @@ jQuery(window).load(function(){
 
 $(document).ready(function(){
 
-	/* ========================================================================= */
-	/*	Menu item highlighting
-	/* ========================================================================= */
-
-	jQuery('#nav').singlePageNav({
-		offset: jQuery('#nav').outerHeight(),
-		filter: ':not(.external)',
-		speed: 1200,
-		currentClass: 'current',
-		easing: 'easeInOutExpo',
-		updateHash: true,
-		beforeStart: function() {
-			console.log('begin scrolling');
-		},
-		onComplete: function() {
-			console.log('done scrolling');
-		}
-	});
-	
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 400) {
-            $("#navigation").css("background-color","#0EB493");
-        } else {
-            $("#navigation").css("background-color","rgba(16, 22, 54, 0.2)");
-        }
-    });
 	
 	/* ========================================================================= */
 	/*	Fix Slider Height
@@ -95,28 +69,6 @@ $(document).ready(function(){
 		closeClick : true,
 	});
 	
-	/* ========================================================================= */
-	/*	Parallax
-	/* ========================================================================= */	
-	
-	$('#facts').parallax("50%", 0.3);
-	
-	/* ========================================================================= */
-	/*	Timer count
-	/* ========================================================================= */
-
-	"use strict";
-    $(".number-counters").appear(function () {
-        $(".number-counters [data-to]").each(function () {
-            var e = $(this).attr("data-to");
-            $(this).delay(6e3).countTo({
-                from: 50,
-                to: e,
-                speed: 3e3,
-                refreshInterval: 50
-            })
-        })
-    });
 	
 	/* ========================================================================= */
 	/*	Back to Top
@@ -139,35 +91,3 @@ $(document).ready(function(){
 });
 
 
-// ==========  START GOOGLE MAP ========== //
-function initialize() {
-    var myLatLng = new google.maps.LatLng(22.402789, 91.822156);
-
-    var mapOptions = {
-        zoom: 14,
-        center: myLatLng,
-        disableDefaultUI: true,
-        scrollwheel: false,
-        navigationControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-        draggable: false,
-        mapTypeControlOptions: {
-            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'roadatlas']
-        }
-    };
-
-    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-
-
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: 'img/location-icon.png',
-        title: '',
-    });
-
-}
-
-google.maps.event.addDomListener(window, "load", initialize);
-// ========== END GOOGLE MAP ========== //
